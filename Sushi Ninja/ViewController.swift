8413db3080b3df98a2a55be4359997715bebac7f
nacicatus <drsaurabhsikka@gmail.com>
2021-03-11 15:01:35 +0100

    Initial Commit

---
+++ Sushi Ninja/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sushi Ninja
+//
+//  Created by Yajnavalkya on 2021. 03. 11..
+//  Copyright © 2021. Yajnavalkya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
